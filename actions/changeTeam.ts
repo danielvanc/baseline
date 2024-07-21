27c0fcaaae3de43fee16d7b17b0ce1e4ae7883f3
Daniel Van Cuylenburg <email@danielvanc.com>
2024-07-21 14:00:44 +0100

    feat: add basic checks on team dropdown

--- actions/changeTeam.ts
+++ actions/changeTeam.ts
@@ -1,17 +1,14 @@
 "use server";
+import { z } from "zod";
+
+const TeamSchema = z.string();
 
 export async function changeTeam(prevState: any, formData: FormData) {
   const team = formData.get("team");
-
-  // TODO: Add Zod error handling
-
-  if (team === "all") {
-    return {
-      team: null,
-    };
-  }
+  const parsed = TeamSchema.safeParse(team);
+  const returnDefault = !parsed.success || team === "all";
 
   return {
-    team,
+    team: returnDefault ? null : team,
   };
 }
